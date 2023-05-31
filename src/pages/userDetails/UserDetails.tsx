d337807db2e6d9370aaa241143899046c1c051f3
Senanyo Benwari <65190458+NightlyHawk@users.noreply.github.com>
2023-05-31 15:48:30 +0100

    styling details page

--- src/pages/userDetails/UserDetails.tsx
+++ src/pages/userDetails/UserDetails.tsx
@@ -29,7 +29,7 @@ export const UserDetail = () => {
     return (
         <div className="text-secondary">
             <div><Link to='/users'>Back to users</Link>
-            <h1>data Details</h1>
+            <h1>User Details</h1>
             <Button variant='btn-outline-error text-error' type="button" >BLACKLIST USER</Button>
             <Button variant="btn-outline-active text-active" type="button" >ACTIVATE USER</Button>
             </div>
@@ -40,17 +40,20 @@ export const UserDetail = () => {
                 {/* {data && Object.entries(data).map(([key, user]) => ( */}
                 {data && 
                   <div>
-                     <ul>
+                    <section className="Top">
+                    <ul className="Top1">
                     <li>
                         <img src={data.profile.avatar} alt="avatar" />
+                    </li>
+                    <li className="left">    
                         <h2>{data.firstName}{data.lastName}</h2>
                         <p>{data.accountNumber}</p>
                     </li>
-                    <li><h3>User's Tier</h3></li>
+                    <li className="right"><h3>User's Tier</h3></li>
                     <li>₦{data.accountBalance}</li>
                     </ul>
                 
-                <ul>
+                <ul className="Top2">
                     <li>General Details</li>
                     <li>Documents</li>
                     <li>Bank Details</li>
@@ -58,43 +61,54 @@ export const UserDetail = () => {
                     <li>Savings</li>
                     <li>App and System</li>
                 </ul>
+                    </section>
               
                
              
-               <div>
+               <section className="bottom">
+               <div className="btm">
                 <h3>Personal Information</h3>
-                <div> <h4>FULL NAME</h4> <p>{data.profile.firstName} {data.profile.lastName}</p></div>
-                <div><h4>PHONE NUMBER</h4> <p>{data.profile.phoneNumber}</p></div>
-                <div><h4>EMAIL ADDRESS</h4> <p>{data.email}</p></div>
-                <div><h4>BVN</h4> <p>{data.profile.bvn}</p></div>
-                <div><h4>GENDER</h4> <p>{data.profile.gender}</p></div>
-                <div><h4>MARITAL STATUS</h4> <p>Single</p></div>
-                <div><h4>CHILDREN</h4> <p>None</p></div>
-                <div><h4>TYPE OF RESIDENCE</h4> <p>Parents Apartment</p></div>
+                <ul>
+                <li> <h4>FULL NAME</h4> <p>{data.profile.firstName} {data.profile.lastName}</p></li>
+                <li><h4>PHONE NUMBER</h4> <p>{data.profile.phoneNumber}</p></li>
+                <li><h4>EMAIL ADDRESS</h4> <p>{data.email}</p></li>
+                <li><h4>BVN</h4> <p>{data.profile.bvn}</p></li>
+                <li><h4>GENDER</h4> <p>{data.profile.gender}</p></li>
+                <li><h4>MARITAL STATUS</h4> <p>Single</p></li>
+                <li><h4>CHILDREN</h4> <p>None</p></li>
+                <li><h4>TYPE OF RESIDENCE</h4> <p>Parents Apartment</p></li>
+                </ul>
                </div>
-               <div>
+               <div className="btm">
                 <h3>Education and Employment</h3>
-                <div><h4>LEVEL OF EDUCATION</h4> <p>{data.education.level}</p></div>
-                <div><h4>EMPLOYMENT STATUS</h4> <p>{data.education.employmentStatus}</p></div>
-                <div><h4>SECTOR OF EMPLOYMENT</h4> <p>{data.education.sector}</p></div>
-                <div><h4>DURATION OF EMPLOYMENT</h4> <p>{data.education.duration}</p></div>
-                <div><h4>OFFICE EMAIL</h4> <p>{data.education.officeEmail}</p></div>
-                <div><h4>MONTHLY INCOME</h4> <p>{data.education.monthlyIncome}</p></div>
-                <div><h4>LOAN REPAYMENT</h4> <p>{data.education.loanRepayment}</p></div>
+                <ul>
+                <li><h4>LEVEL OF EDUCATION</h4> <p>{data.education.level}</p></li>
+                <li><h4>EMPLOYMENT STATUS</h4> <p>{data.education.employmentStatus}</p></li>
+                <li><h4>SECTOR OF EMPLOYMENT</h4> <p>{data.education.sector}</p></li>
+                <li><h4>DURATION OF EMPLOYMENT</h4> <p>{data.education.duration}</p></li>
+                <li><h4>OFFICE EMAIL</h4> <p>{data.education.officeEmail}</p></li>
+                <li><h4>MONTHLY INCOME</h4> <p>{data.education.monthlyIncome}</p></li>
+                <li><h4>LOAN REPAYMENT</h4> <p>{data.education.loanRepayment}</p></li>
+                </ul>
                </div>
-               <div>
+               <div className="btm">
                 <h3>Socials</h3>
-                <div><h4>TWITTER</h4> <p>{data.socials.twitter}</p></div>
-                <div><h4>FACEBOOK</h4> <p>{data.socials.facebook}</p></div>
-                <div><h4>INSTAGRAM</h4> <p>{data.socials.instagram}</p></div>
+               <ul>
+               <li><h4>TWITTER</h4> <p>{data.socials.twitter}</p></li>
+                <li><h4>FACEBOOK</h4> <p>{data.socials.facebook}</p></li>
+                <li><h4>INSTAGRAM</h4> <p>{data.socials.instagram}</p></li>
+               </ul>
                </div>
-               <div>
+               <div className="btm">
                 <h3>Guarantor</h3>
-                <div> <h4>FULL NAME</h4> <p>{data.guarantor.firstName} {data.guarantor.lastName}</p></div>
-                <div><h4>PHONE NUMBER</h4> <p>{data.guarantor.phoneNumber}</p></div>
-                <div><h4>EMAIL ADDRESS</h4> <p></p></div>
-                <div><h4>RELATIONSHIP</h4> <p></p></div>
+                <ul>
+                <li> <h4>FULL NAME</h4> <p>{data.guarantor.firstName} {data.guarantor.lastName}</p></li>
+                <li><h4>PHONE NUMBER</h4> <p>{data.guarantor.phoneNumber}</p></li>
+                <li><h4>EMAIL ADDRESS</h4> <p></p></li>
+                <li><h4>RELATIONSHIP</h4> <p></p></li>
+                </ul>
                </div>
+               </section>
                   </div>}
  {/* ))} */}
            

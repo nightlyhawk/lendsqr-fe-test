cb3d18a2899567e09ff69a65faa502e92d059847
Senanyo Benwari <65190458+NightlyHawk@users.noreply.github.com>
2023-05-31 14:34:29 +0100

    data transfer

--- src/pages/userDetails/UserDetails.tsx
+++ src/pages/userDetails/UserDetails.tsx
@@ -7,7 +7,7 @@ import { user } from "../users/Users";
 
 // type IdProps = {
 //     id: number
-//     user: user
+//     data: data
 // }
 
 type LocationProps = {
@@ -20,16 +20,16 @@ type LocationProps = {
 export const UserDetail = () => {
     const location = useLocation() as unknown as LocationProps;
     const state = location.state
-    //const user: UserProp
+    //const data: dataProp
 
-    const [ loading, data, error, request ] = useAxios<user[]>(
-     {method: 'GET', url:`https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${state}`}
+    const [ loading, data, error, request ] = useAxios<user>(
+     {method: 'GET', url:`https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/datas/${state}`}
      );
     console.log(data)
     return (
         <div className="text-secondary">
-            <div><Link to='/users'>Back to Users</Link>
-            <h1>User Details</h1>
+            <div><Link to='/users'>Back to users</Link>
+            <h1>data Details</h1>
             <Button variant='btn-outline-error text-error' type="button" >BLACKLIST USER</Button>
             <Button variant="btn-outline-active text-active" type="button" >ACTIVATE USER</Button>
             </div>
@@ -37,16 +37,17 @@ export const UserDetail = () => {
                 {error && <div>{error}</div>}
                 {loading && <div>loading...</div>}
 
-                {data && Object.entries(data).map(([key, user]) => (
+                {/* {data && Object.entries(data).map(([key, user]) => ( */}
+                {data && 
                   <div>
                      <ul>
                     <li>
-                        <img src={user && user.profile.avatar} alt="avatar" />
-                        <h2>{user.firstName}{user.lastName}</h2>
-                        <p>{user.accountNumber}</p>
+                        <img src={data.profile.avatar} alt="avatar" />
+                        <h2>{data.firstName}{data.lastName}</h2>
+                        <p>{data.accountNumber}</p>
                     </li>
                     <li><h3>User's Tier</h3></li>
-                    <li>₦{user.accountBalance}</li>
+                    <li>₦{data.accountBalance}</li>
                     </ul>
                 
                 <ul>
@@ -62,40 +63,40 @@ export const UserDetail = () => {
              
                <div>
                 <h3>Personal Information</h3>
-                <div> <h4>FULL NAME</h4> <p>{user.profile.firstName} {user.profile.lastName}</p></div>
-                <div><h4>PHONE NUMBER</h4> <p>{user.profile.phoneNumber}</p></div>
-                <div><h4>EMAIL ADDRESS</h4> <p>{user.email}</p></div>
-                <div><h4>BVN</h4> <p>{user.profile.bvn}</p></div>
-                <div><h4>GENDER</h4> <p>{user.profile.gender}</p></div>
+                <div> <h4>FULL NAME</h4> <p>{data.profile.firstName} {data.profile.lastName}</p></div>
+                <div><h4>PHONE NUMBER</h4> <p>{data.profile.phoneNumber}</p></div>
+                <div><h4>EMAIL ADDRESS</h4> <p>{data.email}</p></div>
+                <div><h4>BVN</h4> <p>{data.profile.bvn}</p></div>
+                <div><h4>GENDER</h4> <p>{data.profile.gender}</p></div>
                 <div><h4>MARITAL STATUS</h4> <p>Single</p></div>
                 <div><h4>CHILDREN</h4> <p>None</p></div>
                 <div><h4>TYPE OF RESIDENCE</h4> <p>Parents Apartment</p></div>
                </div>
                <div>
                 <h3>Education and Employment</h3>
-                <div><h4>LEVEL OF EDUCATION</h4> <p>{user.education.level}</p></div>
-                <div><h4>EMPLOYMENT STATUS</h4> <p>{user.education.employmentStatus}</p></div>
-                <div><h4>SECTOR OF EMPLOYMENT</h4> <p>{user.education.sector}</p></div>
-                <div><h4>DURATION OF EMPLOYMENT</h4> <p>{user.education.duration}</p></div>
-                <div><h4>OFFICE EMAIL</h4> <p>{user.education.officeEmail}</p></div>
-                <div><h4>MONTHLY INCOME</h4> <p>{user.education.monthlyIncome}</p></div>
-                <div><h4>LOAN REPAYMENT</h4> <p>{user.education.loanRepayment}</p></div>
+                <div><h4>LEVEL OF EDUCATION</h4> <p>{data.education.level}</p></div>
+                <div><h4>EMPLOYMENT STATUS</h4> <p>{data.education.employmentStatus}</p></div>
+                <div><h4>SECTOR OF EMPLOYMENT</h4> <p>{data.education.sector}</p></div>
+                <div><h4>DURATION OF EMPLOYMENT</h4> <p>{data.education.duration}</p></div>
+                <div><h4>OFFICE EMAIL</h4> <p>{data.education.officeEmail}</p></div>
+                <div><h4>MONTHLY INCOME</h4> <p>{data.education.monthlyIncome}</p></div>
+                <div><h4>LOAN REPAYMENT</h4> <p>{data.education.loanRepayment}</p></div>
                </div>
                <div>
                 <h3>Socials</h3>
-                <div><h4>TWITTER</h4> <p>{user.socials.twitter}</p></div>
-                <div><h4>FACEBOOK</h4> <p>{user.socials.facebook}</p></div>
-                <div><h4>INSTAGRAM</h4> <p>{user.socials.instagram}</p></div>
+                <div><h4>TWITTER</h4> <p>{data.socials.twitter}</p></div>
+                <div><h4>FACEBOOK</h4> <p>{data.socials.facebook}</p></div>
+                <div><h4>INSTAGRAM</h4> <p>{data.socials.instagram}</p></div>
                </div>
                <div>
                 <h3>Guarantor</h3>
-                <div> <h4>FULL NAME</h4> <p>{user.guarantor.firstName} {user.guarantor.lastName}</p></div>
-                <div><h4>PHONE NUMBER</h4> <p>{user.guarantor.phoneNumber}</p></div>
+                <div> <h4>FULL NAME</h4> <p>{data.guarantor.firstName} {data.guarantor.lastName}</p></div>
+                <div><h4>PHONE NUMBER</h4> <p>{data.guarantor.phoneNumber}</p></div>
                 <div><h4>EMAIL ADDRESS</h4> <p></p></div>
                 <div><h4>RELATIONSHIP</h4> <p></p></div>
                </div>
-                  </div>
- ))}
+                  </div>}
+ {/* ))} */}
            
         </div>
 
